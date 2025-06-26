d41a576993a869063ab309d6a061d13a615ef055
Victor Yuan <victor.2wy@gmail.com>
2025-06-26 16:38:46 -0700

    fix typo in winning condition

--- custom.js
+++ custom.js
@@ -67,7 +67,7 @@ function Gameboard() {
 
       // 3 across diag
       [[0, 0], [1, 1], [2, 2]],
-      [[0, 2], [1, 1], [0, 2]]
+      [[2, 0], [1, 1], [0, 2]]
     ];
 
     // iterate across each winning condition coordinates
